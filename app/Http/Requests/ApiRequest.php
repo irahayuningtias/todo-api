45d5b22dc099a6adcd58139b5bd7d23098d3decb
irahayuningtias <2031710085@student.polinema.ac.id>
2021-11-16 16:34:46 +0700

    JS11 - Belum selesai

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse; 
+
+    abstract public function rules();
+    
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+}
